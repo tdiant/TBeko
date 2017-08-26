56800b007517098837c45b06cd1250ee4abc6a24
tdiant <tdiant@126.com>
2017-08-26 22:41:07 +0800

    Add Launcher Project

---
+++ Launcher/src/main/java/tdiant/tbeko/launcher/TBLauncher.java
@@ -0,0 +1,12 @@
+package tdiant.tbeko.launcher;
+
+/**
+ * Created by tdiant on 2017/8/26.
+ */
+public class TBLauncher {
+    public static void main(String[] args){
+        for(String arg:args){
+            //
+        }
+    }
+}
